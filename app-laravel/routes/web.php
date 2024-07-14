6feed33e0c8e5b442d0548717514834703b632ec
PonceDev <enterprisepublic9@gmail.com>
2024-07-14 16:25:21 -0600

    Agregando carpeta Programación

--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -1,13 +1,18 @@
-<?php
+//<?php
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
 use App\Livewire\Unidad\UnidadHome;
 
+
+
+
 Route::get('/', function () {
-    return view('welcome');
+   return view('welcome');
 });
 
 Route::get('/counter', Counter::class);
 
 Route::get('/unidad', UnidadHome::class);
+
+
