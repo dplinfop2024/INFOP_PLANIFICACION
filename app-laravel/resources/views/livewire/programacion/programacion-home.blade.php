41903ba78fd785ea6d597e058061e8f35f60557d
PonceDev <enterprisepublic9@gmail.com>
2024-07-16 14:13:06 -0600

    Update programacion-home.blade.php

--- app-laravel/resources/views/livewire/programacion/programacion-home.blade.php
+++ app-laravel/resources/views/livewire/programacion/programacion-home.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="d-grid gap-2 col-6 mx-auto">
-    <h5>Menú Programación</h5>
+    
     <h3 class="text-center">POA</h3>
     
     <button class="btn btn-primary" type="button" href="{{route('programacion.listapoa')}}" wire:navigate>Ver Lista POA</button>
