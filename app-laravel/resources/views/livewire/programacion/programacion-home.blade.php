f032261653cb45143f4efeabde49692799951f9a
PonceDev <enterprisepublic9@gmail.com>
2024-07-16 13:04:19 -0600

    Update programacion-home.blade.php

--- app-laravel/resources/views/livewire/programacion/programacion-home.blade.php
+++ app-laravel/resources/views/livewire/programacion/programacion-home.blade.php
@@ -2,11 +2,11 @@
     <h5>Menú Programación</h5>
     <h3>POA</h3>
     
-    <button href="{{route('programacion.listapoa')}}" wire:navigate>Ver Lista POA</button>
+    <button class="btn btn-primary" type="button" href="{{route('programacion.listapoa')}}" wire:navigate>Ver Lista POA</button>
     
     
     <h3>Presupuesto</h3>
-    <button href="{{route('programacion.listapresupuesto')}}" wire:navigate>Ver Lista Presupuesto</button>
+    <button class="btn btn-primary" type="button" href="{{route('programacion.listapresupuesto')}}" wire:navigate>Ver Lista Presupuesto</button>
     
     <!-- Este es un comentario de una sola línea -->
 
