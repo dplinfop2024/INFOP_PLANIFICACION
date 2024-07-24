a4900efbc50951d0f4ef61929835a396a8ef4905
gersondarc <gersondarc@gmail.com>
2024-07-24 13:11:49 -0600

    Lista de POA

--- app-laravel/resources/views/livewire/evaluacion/poa/lista-poa.blade.php
+++ app-laravel/resources/views/livewire/evaluacion/poa/lista-poa.blade.php
@@ -1,3 +1,84 @@
 <div>
     <h1>Lista POA</h1>
+    <table class="table">
+        <thead>
+          <tr>
+              <th scope="col">ID</th>
+              <th scope="col">NOMBRE DE LA UNIDAD</th>
+              <th scope="col">TIPO DE UNIDAD</th>
+              <th scope="col">AÑOS</th>
+              <th scope="col">FECHA</th>
+              <th scope="col">ACENTADO</th>
+              <th scope="col">APROBADO</th>
+              <th scope="col">VER</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+              <th scope="row">1</th>
+              <td>TURISMO</td>
+              <td>Operativo</td>
+              <td>2023</td>
+              <td>01/02/2023</td>
+              <td>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                    <label class="form-check-label" for="defaultCheck1">
+                      
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck2" disabled>
+                    <label class="form-check-label" for="defaultCheck2">
+                      
+                    </label>
+                  </div>
+              </td>
+              <td></td>
+              <td><button type="button" class="btn btn-warning">Detalles</button></td>
+          </tr>
+          <tr>
+              <th scope="row">2</th>
+              <td>DVISION DE PLANIFICACION</td>
+              <td>Administrativo</td>
+              <td>2023</td>
+              <td>04/03/2023</td>
+              <td></td>
+              <td></td>
+              <td><button type="button" class="btn btn-warning">Detalles</button></td>
+          </tr>
+          <tr>
+                <th scope="row">3</th>
+                <td>DEPARTAMENTO DE PROGRAMACION</td>
+                <td>Administrativo</td>
+                <td>2023</td>
+                <td>12/06/2023</td>
+                <td></td>
+                <td></td>
+                <td><button type="button" class="btn btn-warning">Detalles</button></td>
+          </tr>
+          <tr>
+               <th scope="row">4</th>
+               <td>INGERIA Y MANTENIMIENTO</td>
+               <td>Administrativo</td>
+               <td>2023</td>
+               <td>05/06/2023</td>
+               <td></td>
+               <td></td>
+               <td><button type="button" class="btn btn-warning">Detalles</button></td>
+         </tr>
+          <tr>
+               <th scope="row">5</th>
+               <td>UNIDAD DE FORMACION SECRETARIAL</td>
+               <td>OPERATVO</td>
+               <td>2023</td>
+               <td>25/07/2023</td>
+               <td></td>
+               <td></td>
+               <td><button type="button" class="btn btn-warning">Detalles</button></td>
+          </tr>
+        </tbody>
+        
+      </table>
 </div>
+
