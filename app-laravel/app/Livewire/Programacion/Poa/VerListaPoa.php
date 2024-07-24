725191951adbb6919ceff2bbc6de98a505420a9c
PonceDev <enterprisepublic9@gmail.com>
2024-07-24 12:59:02 -0600

    ver lista poa

--- app-laravel/app/Livewire/Programacion/Poa/VerListaPoa.php
+++ app-laravel/app/Livewire/Programacion/Poa/VerListaPoa.php
@@ -6,9 +6,30 @@ use Livewire\Component;
 
 class VerListaPoa extends Component
 {
+    public $poas = [
+        [
+            'no_actividad' => '1',
+            'nombre_unidad' => 'Desarrollo Empresarial',
+            'tipo_unidad' => 'Unidad Operativa',
+            'fecha' => '2025-01-01',
+            'aceptado' => true,
+            'aprobado' => false,
+        ],
+        [
+            'no_actividad' => '2',
+            'nombre_unidad' => 'Recursos Humanos',
+            'tipo_unidad' => 'Unidad Operativa',
+            'fecha' => '2025-02-01',
+            'aceptado' => true,
+            'aprobado' => true,
+        ],
+        // Agrega más POAs según sea necesario
+    ];
+
     public function render()
     {
-        return view('livewire.programacion.poa.ver-lista-poa');
+        return view('livewire.programacion.poa.ver-lista-poa', [
+            'poas' => $this->poas,
+        ]);
     }
 }
-
--- app-laravel/resources/views/livewire/programacion/poa/ver-lista-poa.blade.php
+++ app-laravel/resources/views/livewire/programacion/poa/ver-lista-poa.blade.php
@@ -1,5 +1,22 @@
-<div> 
+<!--<div> 
     <h1>Lista POA Programacion</h1>
-        <! -- y se cierra con -->
-    </div>
-    
+        
+    <!- </div> -->
+
+    <div>
+    <label for="poa">Seleccionar POA:</label>
+    <select id="poa" class="form-control">
+        <option value="">Seleccionar POA</option>
+        @foreach ($poas as $poa)
+            <option value="{{ $poa['no_actividad'] }}">
+                {{ $poa['no_actividad'] }} - {{ $poa['nombre_unidad'] }} - {{ $poa['tipo_unidad'] }}
+            </option>
+        @endforeach
+    </select>
+
+    <!- Botón para detalles -->
+    <button type="button" wire:click="verDetalles">Detalles</button>
+
+    <!-- Detalles del POA seleccionado -->
+    {{-- Aquí puedes mostrar los detalles del POA seleccionado si es necesario --}}
+</div>
