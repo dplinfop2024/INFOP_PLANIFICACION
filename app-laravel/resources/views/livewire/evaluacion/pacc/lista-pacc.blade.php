000a6685056ef1805f1f2caf8f8dcb4b54e61e09
gersondarc <gersondarc@gmail.com>
2024-07-24 22:04:22 -0600

    LISTA DE PACC

--- app-laravel/resources/views/livewire/evaluacion/pacc/lista-pacc.blade.php
+++ app-laravel/resources/views/livewire/evaluacion/pacc/lista-pacc.blade.php
@@ -1,27 +1,21 @@
 <div>
-    <h1>Lista PACC</h1>
-    <table class="table table-striped table-hover table-bordered">
+    <h1 class="text-center">Lista PACC</h1>
+    <table class="table table-striped table-hover table-bordered text-center">
         <thead>
           <tr>
-            <th scope="col">ID</th>
+            <th scope="col">N. UNIDAD</th>
               <th scope="col">NOMBRE DE LA UNIDAD</th>
               <th scope="col">TIPO DE UNIDAD</th>
-              <th scope="col">AÑOS</th>
-              <th scope="col">FECHA</th>
-              <th scope="col">ACEPTADO</th>
-              <th scope="col">APROBADO</th>
+              <th scope="col">AÑO</th>
               <th scope="col">VER</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">1</th>
-              <td>TURISMO</td>
+            <th scope="row">8217</th>
+              <td>UAPA OLANCHITO</td>
               <td>Operativo</td>
-              <td>2023</td>
-              <td>01/02/2023</td>
-              <td>SI</td>
-              <td>SI</td>
+              <td>2024</td>
               <td><button type="button" class="btn btn-warning">Detalles</button></td>
           </tr>
         
