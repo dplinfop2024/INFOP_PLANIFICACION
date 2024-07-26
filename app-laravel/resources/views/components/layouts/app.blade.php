9b40f5b173dabda8f4aa7f0b5da84b51381356b6
KevinFMO <fernandomatamoros963@gmail.com>
2024-07-26 10:12:54 -0600

    Evaluacion: Lista de presupuesto

--- app-laravel/resources/views/components/layouts/app.blade.php
+++ app-laravel/resources/views/components/layouts/app.blade.php
@@ -6,11 +6,25 @@
         <title>{{ $title ?? 'Page Title' }}</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <!-- DATATABLE -->
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css">
         @vite('resources/sass/app.scss','resources/js/app.js')
     </head>
     <body>
+    <!-- DATATABLES ------------------------------------------------------------------------ -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
+    <!-- -------------------------------------------------------------------------------------- -->
+
         <livewire:nav-bar/>
         {{ $slot }}
+        
         <script>
             document.addEventListener('livewire:init', () => {
               Livewire.on('log', (event) => {
@@ -23,5 +37,6 @@
               });
             });
           </script>
+          
     </body>
 </html>
