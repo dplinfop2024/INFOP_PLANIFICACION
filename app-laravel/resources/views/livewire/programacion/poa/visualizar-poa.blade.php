a590474dd0c8707657610dc3049b60b87c055a16
PonceDev <enterprisepublic9@gmail.com>
2024-07-26 11:32:59 -0600

    Update visualizar-poa.blade.php

--- app-laravel/resources/views/livewire/programacion/poa/visualizar-poa.blade.php
+++ app-laravel/resources/views/livewire/programacion/poa/visualizar-poa.blade.php
@@ -19,94 +19,176 @@
         .small-font {
             font-size: 10px; /* Cambia el tamaño de la fuente */
         }
+        .footer-info {
+            font-size: 0.8em; /* Tamaño de fuente más pequeño para la información adicional */
+            margin-top: 20px; /* Espaciado superior */
+        }
+        .footer-info label {
+            display: inline-block;
+            width: 200px; /* Ancho fijo para alinear las etiquetas */
+        }
+        .footer-info input {
+            border: 1px solid #ccc; /* Borde para los campos de entrada */
+            width: 150px; /* Ancho de los campos de entrada */
+            margin-right: 10px; /* Espaciado entre campos */
+        }
     </style>
-    <title>Plan Operativo Anual</title>
+    <title>Plan Operativo de Administracion</title>
 </head>
 <body>
 
-
 <div>
-    <h1 class="text-center">PLAN OPERATIVO PRESUPUESTO</h1>
-    <h1 class="text-center">Matriz de Planificacion Unidades Operativas</h1>
-    <h1 class="text-center">Año 2025</h1>
+    <h5 class="text-center">PLAN OPERATIVO PRESUPUESTO</h5>
+    <h6 class="text-center">Matriz de Planificacion Unidades Operativas</h6>
+    <h6 class="text-center">Año 2025</h6>
 
-    <label><b>Nombre de la Unidad:</b> Ingeniería y Mantenimiento</label>
-    <br><label><b>No Actividad:</b> 1005</label></br>
+    <label><b>Nombre de la Unidad:</b> Ingeniería y Mantenimiento</label>            
     <label><b>Nombre del responsable:</b> </label>
-    <br><label><b>Cargo</b> 6550</label></br>
-  
+    <br><label><b>No Actividad:</b> 1005</label><br>  
+    <br><label><b>Cargo:</b></label></br>
+
     <table class="table table-bordered border border-gray small-font">
-       <!-- EN CABEZADO DE LA TABLA-->
+        <!-- EN CABEZADO DE LA TABLA -->
         <thead class="table-primary text-center">
             <tr>
-              <th colspan="2" scope="col">COMPONENTE</th>
-              <th colspan="2" scope="col">LINEA ESTRATEGIA</th>
-              <th colspan="2" scope="col">META</th>
-              <th rowspan="0" scope="col">UNIDAD DE MEDIDA</th>
-              <th colspan="3" scope="col">1° TRIMESTRE</th>
-              <th colspan="3" scope="col">2° TRIMESTRE</th>
-              <th colspan="3" scope="col">3° TRIMESTRE</th>
-              <th colspan="3" scope="col">4° TRIMESTRE</th>
-              <th scope="col">PROGRAMACION</th>
-              <th rowspan="0">Presupuesto estimado por Meta</th>
-           </tr>
-           <tr>
-              <th scope="col">Numero</th>
-              <th scope="col">Nombre</th>
-              <th scope="col">Numero</th>
-              <th scope="col">Descripcion</th>
-              <th scope="col">Codigo</th>
-              <th scope="col">Resultado de Produccion</th>
-              <th colspan="0" scope="col"> 1</th>   
-              <th scope="col">2</th>
-              <th scope="col">3</th>
-              <th scope="col">4</th>
-              <th scope="col">5</th>
-              <th scope="col">6</th>
-              <th scope="col">7</th>
-              <th scope="col">8</th>
-              <th scope="col">9</th>
-              <th scope="col">10</th>
-              <th scope="col">11</th>
-              <th scope="col">12</th>
-              <th scope="col">Anual</th>
-              
-          </tr>
-        </thead>
-        <!-- FILAS DE LAS TABLA-->
-        <tbody class="text-justify">
-          <tr>
-              <th scope="row">3</th>
-              <td>INFRAESTRUCTURA Y ORNATO</td>
-              <td>3.1</td>
-              <td>
-                    Mejorar la infraestructura actual de las instalaciones del INFOP a nivel nacional con el fin de contar con espacios adecuados que mejoren las condiciones pedagógicas para aprendices y coolaboradores. 
-              </td>
-              <td>3.1.1</td>
-              <td>FORMULACION Y POSTERIORMENTE CONSTRUCCION DE TALLERES DE FORMACION PROFESIONAL, LOS ENCUENTROS, FRANCISCO MORAZAN (ESTUDIO)
-              </td>
-              <td>Contrucciones y remodelaciones</td>
-              <td></td>
-              <td></td>
-              <td>1</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>1</td>
-              <td>L12,600,000.00</td>
+                <th colspan="2" scope="col">COMPONENTE</th>
+                <th colspan="2" scope="col">LINEA ESTRATEGIA</th>
+                <th colspan="2" scope="col">META</th>
+                <th rowspan="0" scope="col">UNIDAD DE MEDIDA</th>
+                <th colspan="3" scope="col">1° TRIMESTRE</th>
+                <th colspan="3" scope="col">2° TRIMESTRE</th>
+                <th colspan="3" scope="col">3° TRIMESTRE</th>
+                <th colspan="3" scope="col">4° TRIMESTRE</th>
+                <th scope="col">PROGRAMACION</th>
+                <th rowspan="0">Presupuesto estimado por Meta</th>
             </tr>
-            
-               
-              
+            <tr>
+                <th scope="col">Numero</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Numero</th>
+                <th scope="col">Descripcion</th>
+                <th scope="col">Codigo</th>
+                <th scope="col">Resultado de Produccion</th>
+                <th colspan="0" scope="col">1</th>   
+                <th scope="col">2</th>
+                <th scope="col">3</th>
+                <th scope="col">4</th>
+                <th scope="col">5</th>
+                <th scope="col">6</th>
+                <th scope="col">7</th>
+                <th scope="col">8</th>
+                <th scope="col">9</th>
+                <th scope="col">10</th>
+                <th scope="col">11</th>
+                <th scope="col">12</th>
+                <th scope="col">Anual</th>
+            </tr>
+        </thead>
+        <!-- FILAS DE LA TABLA -->
+        <tbody class="text-justify">
+            <tr>
+                <th scope="row">3</th>
+                <td>INFRAESTRUCTURA Y ORNATO</td>
+                <td>3.1</td>
+                <td>Mejorar la infraestructura actual de las instalaciones del INFOP a nivel nacional con el fin de contar con espacios adecuados que mejoren las condiciones pedagógicas para aprendices y colaboradores.</td>
+                <td>3.1.1</td>
+                <td>FORMULACION Y POSTERIORMENTE CONSTRUCCION DE TALLERES DE FORMACION PROFESIONAL, LOS ENCUENTROS, FRANCISCO MORAZAN (ESTUDIO)</td>
+                <td>Construcciones y remodelaciones</td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td>L12,600,000.00</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>3.1.2</td>
+                <td>FORMULACION Y POSTERIORMENTE CONSTRUCCION DE TALLERES DE FORMACION PROFESIONAL, COMAYAGUA, COMAYAGUA (ESTUDIO)</td>
+                <td>Construcciones y remodelaciones</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td>L1,260,000.00</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>3.1.3</td>
+                <td>FORMULACION Y POSTERIORMENTE CONSTRUCCION DE CERCO PERIMETRAL INFOP TRUJILLO</td>
+                <td>Construcciones y remodelaciones</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td>L5,000,000.00</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>3.1.4</td>
+                <td>FORMULACION Y POSTERIORMENTE CONSTRUCCION DE TALLERES DE INFOP TRUJILLO</td>
+                <td>Construcciones y remodelaciones</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td>L30,000,000.00</td>
+            </tr>
         </tbody>
-      </table>
-  </div>
+    </table>
   
-  
-  
+    <!-- Información adicional debajo de la tabla -->
+    <div class="footer-info">
+        <div class="info-item">
+            <b>Validado por:</b> [Nombre del validador]
+        </div>
+        <div class="info-item">
+            <b>Fecha de validación:</b> [__/__/____]
+        </div>
+    </div>
+</body>
+</html>
