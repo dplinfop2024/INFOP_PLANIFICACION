030b2fd9b5dd5daf8b42fa47118886a413e1e700
gersondarc <gersondarc@gmail.com>
2024-08-05 11:08:33 -0600

    Model Gastos Pacc

--- app/Livewire/Evaluacion/Pacc/VerPacc.php
+++ app/Livewire/Evaluacion/Pacc/VerPacc.php
@@ -3,8 +3,7 @@
 namespace App\Livewire\Evaluacion\Pacc;
 use App\Models\Pacc;
 use App\Models\User;
-use APP\Models\GastoPacc;
-use App\Models\Prueba;
+use App\Models\GastoPacc;
 use Livewire\Component;
 
 class VerPacc extends Component
@@ -17,7 +16,7 @@ class VerPacc extends Component
     public function mount (){
          $this->pacc=Pacc::find(1);
          $this->user=User::find($this->pacc->id_usuario);
-         $this->gastopacc= Prueba::where('id_pacc', $this->pacc->id)->get();
+         $this->gastopacc= GastoPacc::where('id_pacc', $this->pacc->id)->get();
          $this->dispatch('log', $this->gastopacc);
 }
     public function render()
