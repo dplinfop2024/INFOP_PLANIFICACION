7591d651fcd1c542fe9b070982d9e57671b21e3f
PonceDev <enterprisepublic9@gmail.com>
2024-08-13 12:58:08 -0600

    Update perfil-usuario.blade.php

--- resources/views/livewire/perfil-usuario.blade.php
+++ resources/views/livewire/perfil-usuario.blade.php
@@ -1,74 +1,76 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">
+
 <div class="container">
-    <div class="main-body">
-      <div class="row gutters-sm">
-        <!-- Columna para los datos personales -->
-        <div class="col-md-9">
-          <div class="card mb-3">
-            <div class="card-body">
-              <h4 class="mb-4 text-center">Datos Personales</h4>
-              <div class="row">
-                <div class="col-sm-4">
-                  <h6 class="mb-0">Nombre Usuario:</h6>
-                </div>
-                <div class="col-sm-8 text-secondary">
-                  Kenneth 
-                </div>
+  <div class="main-body">
+    <div class="row gutters-sm">
+      <!-- Columna para los datos personales -->
+      <div class="col-md-9">
+        <div class="card mb-3">
+          <div class="card-body">
+            <h4 class="mb-4 text-center">Datos Personales</h4>
+            <div class="row">
+              <div class="col-sm-4">
+                <h6 class="mb-0">Nombre Usuario:</h6>
               </div>
-              <hr>
-              <div class="row">
-                <div class="col-sm-4">
-                  <h6 class="mb-0">Nombre:</h6>
-                </div>
-                <div class="col-sm-8 text-secondary">
-                  Kenneth Valdez
-                </div>
+              <div class="col-sm-8 text-secondary">
+                Kenneth 
               </div>
-              <hr>
-              <div class="row">
-                <div class="col-sm-4">
-                  <h6 class="mb-0">Nombre Unidad:</h6>
-                </div>
-                <div class="col-sm-8 text-secondary">
-                  programacion
-                </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-4">
+                <h6 class="mb-0">Nombre:</h6>
               </div>
-              <hr>
-              <div class="row">
-                <div class="col-sm-4">
-                  <h6 class="mb-0">Cargo:</h6>
-                </div>
-                <div class="col-sm-8 text-secondary">
-                  jefe
-                </div>
+              <div class="col-sm-8 text-secondary">
+                Kenneth Valdez
               </div>
-              <hr>
-              <div class="row">
-                <div class="col-sm-4">
-                  <h6 class="mb-0">Correo:</h6>
-                </div>
-                <div class="col-sm-8 text-secondary">
-                  ejemplo@yahoo.com
-                </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-4">
+                <h6 class="mb-0">Nombre Unidad:</h6>
               </div>
-              <hr>
-              <div class="row">
-                <div class="col-sm-12">
-                  <a class="btn btn-info btn-sm" target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
-                </div>
+              <div class="col-sm-8 text-secondary">
+                programacion
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-4">
+                <h6 class="mb-0">Cargo:</h6>
+              </div>
+              <div class="col-sm-8 text-secondary">
+                jefe
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-4">
+                <h6 class="mb-0">Correo:</h6>
+              </div>
+              <div class="col-sm-8 text-secondary">
+                ejemplo@yahoo.com
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-12">
+                <a class="btn btn-info btn-sm" target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
               </div>
             </div>
           </div>
         </div>
-  
-        <!-- Columna para la imagen -->
-        <div class="col-md-3 mb-3">
-          <div class="card">
-            <div class="card-body d-flex justify-content-end position-relative">
-              <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle profile-img">
-            </div>
+      </div>
+
+      <!-- Columna para la imagen -->
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <div class="card-body d-flex justify-content-center align-items-center">
+            <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-thumbnail" alt="avatar">
           </div>
         </div>
       </div>
     </div>
   </div>
-  
+</div>
